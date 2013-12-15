4c6f4d9ead2cca1b8e9248192f3f181f3c70edc8
aert <dev.aert@gmail.com>
2013-12-15 18:13:13 +0100

    Better color for spendings.

--- assets/association/templates/admin/dashboard/includes/_highchart_theme.js
+++ assets/association/templates/admin/dashboard/includes/_highchart_theme.js
@@ -5,6 +5,6 @@
  */
 
 colors_earning = ['#003300', '#006600', '#009900', '#00CC00', '#00FF00'].reverse().concat(['#333300', '#336600', '#339900', '#33CC00'].reverse())
-colors_spending = ['#FF0000', '#FF3300', '#FF6600', '#FF9900', '#FFCC00'].reverse().concat(['#FFFF00', '#FF0033', '#FF3333', '#FF6633'].reverse())
+colors_spending = ['#B40404', '#FF3300', '#FF6600', '#FF9900', '#FFCC00'].reverse().concat(['#FFFF00', '#FF0033', '#FF3333', '#FF6633'].reverse())
 
 
